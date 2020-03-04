2f788c794045cf84664db4257ee2d3900ef1ae7c
prathamesh-p11 <prathameshp2472@gmail.com>
2020-03-03 19:26:06 -0800

    indec page - raw

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <html>
 	<head>
+		<meta charset = "UTF-8">
+		<title>Bughound main</title>
+	</head>
+
+	<body>
+		<h1>WELCOME</h1>
 	
+		<table>
+			<tr><td>
+				<input type="button" onclick="window.location.href = './login/login.php';" value="Login", id="bug"/>		
+			</td></tr>
+			<tr><td>
+				<input type="button" onclick="window.location.href = './register/register.php';" value="Register", id="bug"/>		
+			</td></tr>
+		</table>
+	</body>
