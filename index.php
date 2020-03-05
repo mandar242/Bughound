91b25ebc13149d8bbf5cdfc4a452160afe573460
prathamesh-p11 <prathameshp2472@gmail.com>
2020-03-04 20:24:46 -0800

    user validation functions added

--- index.php
+++ index.php
@@ -5,14 +5,28 @@
 	</head>
 
 	<body>
-		<h1>WELCOME</h1>
+		<h1>Bughound</h1>
+		<?php
+			include 'auth/validateUser.php';
+			checkLogin();
+		?>
+
+		<script language=Javascript>
+			//if logged in user not same as new user =>
+			function UserLogOut()
+			{
+				//delete all cookies
+				document.cookie.split(";").forEach(function(f)
+				{
+					document.cookie = f.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date.toUTCString() + ";path=/");
+				});
+
+				window.location.replace("login.php")
+				//display login window after successful logout
+			}
+		</script>
 	
 		<table>
-			<tr><td>
-				<input type="button" onclick="window.location.href = './login/login.php';" value="Login", id="bug"/>		
-			</td></tr>
-			<tr><td>
-				<input type="button" onclick="window.location.href = './register/register.php';" value="Register", id="bug"/>		
-			</td></tr>
+			<!--buttons to add emp prog and areas-->
 		</table>
 	</body>
---
+++ validateUser.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	function checkLogin(){
+		if(!isset($_SESSION['login'])) {
+			echo "<SCRIPT type='text/javascript'>
+			alert('User not logged in!!!!');
+			document.location.href='/bughound544/login.php';
+			</SCRIPT>";	
+		}	
+	}
+	function isAdmin() {
+		$user_level =  (int)$_SESSION["user_level"];
+		if($user_level < 2) {
+			return false;
+		}
+		return true;
+	}
+	function checkLevel($min_level){
+		$user_level =  (int)$_SESSION["user_level"];
+		if($user_level < $min_level) {
+			echo "<SCRIPT type='text/javascript'>
+				alert('User level not sufficient!!!!');
+			</SCRIPT>";	
+			return false;
+		}
+		return true;
+	}
+?>
