c85a409e6335f26c4b04dcc0c299063ac084e662
prathamesh-p11 <prathameshp2472@gmail.com>
2020-03-10 19:04:52 -0700

    Program check and display

--- addArea.php
+++ addArea.php
@@ -9,28 +9,50 @@
     <body>
     <div class= "jumbotron">
     <div class= "container">
-		<?php
-			include 'validateUser.php';
-			checkLogin();
-		?>
-		
+        <?php
+            include 'validateUser.php';
+            checkLogin();
+        ?>
+        
         <h1>Add New Area</h1>
         <form action="addUtil.php" method="post" onsubmit="return validate(this)">
             <table>
                 <tr><td><input type="Text" name="area_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Area Name"</td></tr>
-            </table>
+            
+            <tr><td>
+            <?php
+                $con = mysqli_connect("localhost","root");
+                mysqli_select_db($con, "Bughound");
+                $sql = "SELECT program_id,program_name from programs";  
+                $result = mysqli_query($con, $sql);
+
+                echo "<select name = 'progid'>";
+                echo "<option value= ''" ."'>Select Program ID" . "</option>";  
+                while($row = mysqli_fetch_array($result)){
+                    echo "<option value='" . $row['program_id'] ."'>" . $row['program_id'] ." - " . $row['program_name'] ."</option>";  
+                }
+                echo "</select>"
+            ?>
+            </td></tr></table>
+        
             <br>
             <input type="submit" name="submit"class="btn btn-primary btn-lg" value="Submit">
-			<input type="button" value="Cancel" class="btn btn-primary btn-lg" onclick="window.location.href = 'areaMain.php'">
+            <input type="button" value="Cancel" class="btn btn-primary btn-lg" onclick="window.location.href = 'areaMain.php'">
 
         </form>
 
         <script language=Javascript>
 
             function validate(theform) {
-                if(theform.area_name.value === ""){
+            if(theform.area_name.value === ""){
                     alert ("Name field cannot be empty");
                     return false;
+                }
+                if(theform.progid.value === ""){
+                    alert ("Program ID field cannot be empty");
+                    return false;
+                }
+            }
 
         </script>
         </div>
